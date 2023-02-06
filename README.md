1c14be43cc5f456b9d95da24abce1464c7204b1c
Gerard <gerardlamo@gmail.com>
2023-02-06 10:15:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Db Masker
+
+Db Masker is a library for masking database data by performing updates on the data and optionally removing it. The updates and removals are specified in yaml configuration files.
+
+### Supports
+
+- Postgres
+- MySQL
+- SQLite
+
+Install the correspond client library to use the database.
+
+## Configuring Db Masker
+To configure Db Masker, you'll need to set up the following:
+
+The database connection information in the config.yaml file.
+```yaml
+tasksDir: "./tasks"
+client: "pg"
+connection:
+  host: "127.0.0.1"
+  user: "user"
+  password: "password"
+  database: "database"
+```
+
+The data updates and removals in one or more yaml files in the `tasks` directory. Each file corresponds to a namespace, and the data updates and removals are specified by table.
+
+Here's an example of a yaml file:
+
+```yaml
+namespace:
+  table:
+    delete: true # remove all data from the table ( can also be a raw where clause eg. "column_name2 > 100" )
+    updates:
+      - column: "column_name"
+        fn: "name.firstName" # use the faker function name.firstName to update the column
+      - column: "column_name2"
+        fn: "random.number" # use the faker function random.number to update the column
+        args: [1, 100] # pass in two arguments to the function, the minimum and maximum numbers
+      - column: "column_name3"
+        fn: "raw" # use the raw sql to update the column
+        query: "CONCAT(column_name, ' ', column_name2)"
+```
+
+## Usage
+
+To run Db Masker, run the following command in your terminal:
+
+```bash
+npx db-masker db-masker-config.yaml
+```
+
+This will mask the data in your database according to the specifications in the yaml configuration files. The output will be logged to the console, and the process will exit with a status code of 0 on success or 1 on failure.
---
+++ src/main.ts
@@ -0,0 +1,155 @@
+import fs from 'fs-extra';
+import jsYaml from 'js-yaml';
+import faker from '@faker-js/faker';
+import path from 'path';
+import Knex from 'knex';
+import type {Knex as IKnex} from 'knex';
+
+type DbConfig = {
+  tasksDir: string;
+  client: string
+  connection: string | object
+}
+
+type FileConfig = {
+  [ns: string]: NSConfig;
+};
+
+type NSConfig = {
+  [table: string]: {
+    delete?: boolean | string;
+    updates: Array<FakerUpdate | RawUpdate>;
+  };
+};
+
+type Update = {
+  column: string;
+  where?: string;
+}
+
+type RawUpdate = Update & {
+  fn: 'raw';
+  query: string;
+}
+
+type FakerUpdate = Update & {
+  fn: string;
+  args?: string[];
+}
+
+async function run(configPath = './config.yaml') {
+  // Get config from arguments
+  
+  const config = await getConfig(configPath);
+  const allTasks = await loadTasks(config.tasksDir);
+
+  const knex = Knex({
+    client: config.client,
+    connection: config.connection,
+  });
+
+  for (const nsTask of Object.keys(allTasks)) {
+    console.log(`Doing tasks for namespace ${nsTask}`);
+    await doTasks(knex, allTasks[nsTask]);
+  }
+}
+
+async function loadTasks(tasksDir = './tasks'): Promise<FileConfig> {
+  // Read config.yaml file for knex connection
+  const taskDir = path.resolve(__dirname, tasksDir);
+  const allFiles = await fs.readdir(taskDir);
+  const tasks: FileConfig = {};
+  for (const file of allFiles) {
+    if (file.endsWith('.yaml')) {
+      const nsTask = loadYaml(path.resolve(taskDir, file));
+      Object.assign(tasks, nsTask);
+    }
+  }
+
+  return tasks;
+}
+
+async function doTasks(
+  knex: IKnex,
+  nsTask: NSConfig
+) {
+  for (const [table, task] of Object.entries(nsTask)) {
+    if (task.delete) {
+      // If delete is a string, then it's a where clause
+      if (typeof task.delete === 'string') {
+        await knex(table).whereRaw(task.delete).delete();
+      } else {
+        await knex(table).delete();
+      }
+    }
+
+    if (task.updates) {
+      for (const update of task.updates) {
+        const query = knex(table);
+        if (update.where) {
+          query.whereRaw(update.where);
+        }
+
+        if (update.fn === 'raw') {
+          await query.update(knex.raw((update as RawUpdate).query));
+        } else {
+          const value = fakeit(update.fn, (update as FakerUpdate).args || []);
+          await query.update(update.column, value);
+        }
+      }
+    }
+  }
+}
+
+/**
+ * Exceute a faker function with given arguments
+ * @param {string} fn possibly dot separated function name
+ * @param {string[]?} args arguments to pass the faker function
+ */
+function fakeit(fn: string, args?: Array<any>) {
+  const fnToCall = fn
+    .split('.')
+    .reduce((f: { [x: string]: any }, part: string | number) => f[part], faker);
+  if (fnToCall instanceof Function) {
+    return fnToCall(...(args || []));
+  } else {
+    throw new Error(`Invalid faker function ${fn}`);
+  }
+}
+
+/**
+ *
+ * @returns {Promise<{
+ *  client: string
+ *  connection: object
+ * }>}
+ */
+function getConfig(configPath = './config.yaml') {
+  return loadYaml(path.resolve(__dirname, configPath)) as Promise<DbConfig>;
+}
+
+/**
+ *
+ * @param {string} filename
+ * @returns {Promise<object>}
+ */
+async function loadYaml(filename: any) {
+  const yamlString = await fs.readFile(filename, 'utf8');
+  return jsYaml.load(yamlString) as object;
+}
+
+
+console.log('Starting db masker');
+if (process.argv.length < 3) {
+  console.log('Usage: db-masker <config.yaml>');
+  process.exit(1);
+}
+
+const configPath = process.argv[2];
+Promise.all([run(configPath)]).then(() => {
+  console.log('Thank you for using db-masker');
+  process.exit(0);
+}).catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
